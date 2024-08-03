f355980f3f6bed10b8dcb4d6e6651821e46c0983
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-03 08:36:46 +0530

    dolby: Introduce graphical equalizer
    Squashed:
    
    dolby: Refresh preset name on main screen
    
    Change-Id: I96783e2a03c384f031787f4cc9140f7d64dadb2f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I38ee6ce594e5671af42afc3d4bf0f004329482b9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

---
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi.geq.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.android.settingslib.spa.framework.theme.SettingsDimension
+import com.android.settingslib.spa.framework.theme.SettingsTheme
+
+@Composable
+fun EqualizerScreen(
+    viewModel: EqualizerViewModel,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(SettingsDimension.itemPadding)
+            .then(modifier),
+        color = SettingsTheme.colorScheme.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            PresetSelector(viewModel = viewModel)
+            EqualizerBands(viewModel = viewModel)
+        }
+    }
+}
