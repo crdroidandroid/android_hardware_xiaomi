7dab44f923c3b22f10ceb6d7181de4e6fc134f72
Abhay Singh Gill <abhaygill017@gmail.com>
2024-11-03 18:23:13 +0530

    dolby: Add icons for dolby profiles
    Also nuke voice profile and fixup some strings.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
+++ dolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
@@ -112,6 +112,7 @@ class DolbySettingsFragment : PreferenceFragment(),
         switchBar.setChecked(dsOn)
 
         profilePref.onPreferenceChangeListener = this
+        updateProfileIcon(profile)
         profilePref.setEnabled(dsOn)
         profilePref.apply {
             if (entryValues.contains(profile.toString())) {
@@ -164,6 +165,7 @@ class DolbySettingsFragment : PreferenceFragment(),
                 val profile = newValue.toString().toInt()
                 dolbyController.profile = profile
                 (preferenceManager.preferenceDataStore as DolbyPreferenceStore).profile = profile
+                updateProfileIcon(profile)
                 updateProfileSpecificPrefs()
             }
 
@@ -282,6 +284,15 @@ class DolbySettingsFragment : PreferenceFragment(),
         hpVirtPref.apply {
             setChecked(dolbyController.getHeadphoneVirtEnabled(currentProfile))
             summary = null
+        }
+    }
+
+    private fun updateProfileIcon(profile: Int) {
+        when (profile) {
+            0 -> profilePref.setIcon(R.drawable.ic_profile_dynamic)
+            1 -> profilePref.setIcon(R.drawable.ic_profile_movie)
+            2 -> profilePref.setIcon(R.drawable.ic_profile_music)
+            else -> profilePref.setIcon(R.drawable.ic_profile_custom)
         }
     }
 
