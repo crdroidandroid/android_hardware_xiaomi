efb44401db954cc9dcf37d0b8beb293f0c34ef9e
Pranav Vashi <neobuddy89@gmail.com>
2025-01-22 20:17:29 +0530

    Revert "dolby: Fix Building in Andoid 15"
    This reverts commit 867bb4b4e63fc2f7b4e3d45870cb1ff034a6fc2d.

--- dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
+++ dolby/src/co/aospa/dolby/xiaomi/geq/ui/EqualizerScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,21 +23,19 @@ fun EqualizerScreen(
     viewModel: EqualizerViewModel,
     modifier: Modifier = Modifier
 ) {
-    SettingsTheme {
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(SettingsDimension.itemPadding)
-                .then(modifier),
-            color = MaterialTheme.colorScheme.background
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(SettingsDimension.itemPadding)
+            .then(modifier),
+        color = SettingsTheme.colorScheme.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxHeight()
         ) {
-            Column(
-                verticalArrangement = Arrangement.Top,
-                modifier = Modifier.fillMaxHeight()
-            ) {
-                PresetSelector(viewModel = viewModel)
-                EqualizerBands(viewModel = viewModel)
-            }
+            PresetSelector(viewModel = viewModel)
+            EqualizerBands(viewModel = viewModel)
         }
     }
 }
